41c46eec607ae04af8778089deccf8fbfed99921
dylanbannon <dylanbannon5@gmail.com>
2021-11-05 13:13:09 -0500

    Initial implementation (#1)

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# github-action-telemetry
-GitHub Action for telemetry harvesting from repos
+# GitHub Action for Telemetry
+
+**Name:** `cloudposse/github-action-telemetry`
+
+This GitHub Action will scrape the event for JIRA tickets and an assortment of relevant metadata and post it to a webhook endpoint
+
+## Usage instructions
+
+Create a workflow file (e.g. `.github/workflows/github-telemetry.yml`) that contains a step that `uses: cloudposse/github-action-telemetry@v1.0.0`. Here's an example workflow file:
+
+```yaml
+name: GitHub Telemetry
+on: pull_request
+    types: [opened]
+jobs:
+  telemetry:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: cloudposse/github-action-telemetry@v1.0.0
+      with:
+        collector_endpoint: "https://awesome-endpoint.net"
+        collector_token: ${{ secrets.token }}
+        jira_keys: "DEV,OPS,SECOPS,IT"
+
+```
+
+## Inputs
+
+- `collector_endpoint`: This is the HTTP(S) address of a webhook that is expecting POST payloads that conform to the specifications in the next section.
+- `collector_token`: This is an optional token the will be passed in a `token=` header attached to the HTTP POST payload sent to the `collector_endpoint`.
+- `jira_keys`: This is a comma-separated, (space-free) list of JIRA codes to look for in the GitHub event JSON. For example, if `jira_keys: "INTERNAL"`, then `cloudposse/github-action-telemetry` will extract all strings matching the regular expression `\bINTERNAL-\d+` from the GitHub event JSON.
+
+## Payload Specification
+
+```json
+{
+  repo_org: // e.g. cloudposse
+  repo_name: // e.g. infra
+  repo_url: // full URL to repo
+  event_type: // github event type
+  jira_tickets: [] // extracted from event payload
+  pull_request { // if the event is a pull_request
+    created_at:
+    number:
+    url:
+    title:
+    author:
+    description:
+  }
+}
+
+```
+
+## Use-case
+
+This event could facilitate automatic annotation of JIRA tickets based on the contents of GitHub pull requests, to improve task tracking between these two platforms when using the native Jira GitHub interaction is not an option.
---
+++ send_webhook_payload.py
@@ -0,0 +1,85 @@
+# send_webhook_payload.py
+# This action extracts metadata from a GitHub Actions event and sends it via HTTP to a database.
+# Note: For now, this action only supports pull request events.
+# Inputs:
+# os.environ['INPUT_COLLECTOR_ENDPOINT']
+# os.environ['INPUT_COLLECTOR_TOKEN']
+# os.environ['INPUT_JIRA_KEYS']
+# os.environ['INPUT_EVENT']
+
+# imports
+import json
+import os
+import re
+import requests
+
+
+def get_pull_request_info(event_json):
+    pull_request_info = {}
+    pull_request_info['number'] = event_json["pull_request"]["number"]
+    pull_request_info['url'] = event_json["pull_request"]["_links"]["html"]["href"]
+    pull_request_info['title'] = event_json["pull_request"]["title"]
+    pull_request_info['created_at'] = event_json["pull_request"]["created_at"]
+    pull_request_info['description'] = event_json["pull_request"]["body"]
+    pull_request_info['author'] = event_json["pull_request"]["user"]["login"]
+    return pull_request_info
+
+
+if __name__ == '__main__':
+    # read in env vars
+    collector_endpoint = os.environ['INPUT_COLLECTOR_ENDPOINT']
+    collector_token = os.environ['INPUT_COLLECTOR_TOKEN']
+    jira_keys = os.environ['INPUT_JIRA_KEYS']
+    event = os.environ['INPUT_EVENT']
+    if collector_endpoint == "" or jira_keys == "" or event == "":
+        if collector_endpoint == "":
+            print("collector_endpoint input is null-valued.")
+        if jira_keys == "":
+            print("jira_keys input is null-valued.")
+        if event == "":
+            print("event input is null-valued.")
+        raise ValueError("Because one or more inputs is null-valued, this program is termiating.")
+
+    # extract all JIRA tickets
+    jira_tickets = []
+    for key in jira_keys.split(","):
+        regex_string = r"(?<=\W|[a-z]){}-\d+".format(key)
+        print(f"regex: {regex_string}")
+        matches = re.findall(regex_string, event)
+        if matches:
+            jira_tickets.append(matches)
+    # flatten lists and deduplicate entries, if needed
+    all_jira_tickets = [item for sublist in jira_tickets for item in sublist]
+    all_jira_tickets = list(set(all_jira_tickets))
+
+    # read in event data into json for remaining queries
+    event_json = json.loads(event)
+
+    # construct payload
+    #  extract general metadata
+    payload = {}
+    payload['repo_org'] = event_json["repository"]["full_name"].split("/")[0]
+    payload['repo_name'] = event_json["repository"]["name"]
+    payload['repo_url'] = event_json["repository"]["html_url"]
+    payload['jira_tickets'] = all_jira_tickets
+    #  extract event type-specific information
+    if "pull_request" in event_json.keys():
+        payload['pull_request'] = get_pull_request_info(event_json)
+        payload['event_type'] = "pull_request"
+        payload['event_action'] = event_json["action"]
+    else:
+        type_specific_info = {}
+        payload['event_type'] = "unsupported"
+
+    # send payload
+    print(f"Payload: {json.dumps(payload, indent=4)}")
+    headers = {}
+    headers['user-agent'] = "cloudposse/github-action-telemetry"
+    if collector_token:
+        headers['token'] = collector_token
+        response = requests.post(collector_endpoint, headers=headers, json=payload)
+    else:
+        response = requests.post(collector_endpoint, headers=headers, json=payload)
+    # check response
+    print(f"Response code: {response.status_code}")
+    response.raise_for_status()
